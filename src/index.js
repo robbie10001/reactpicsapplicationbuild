08319f2df1bcee26c7c1f388b1640f480c90b289
robbie10001/rn <robbie_c1000@hotmail.com>
2019-12-26 12:48:43 +1100

    First lesson and application overview

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom"; 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+WHAT ARE WE GOING TO BE WORKING ON IN THIS APPLICATION
+1. How do we get feedback from the user? USER EVENTS! How do we deal with user events?
+2. How do we fetch data from some outside API or server? How to we pull data from a database?
+3. How do we show lists of records? 
+
+/*
+
+/*
+OUR BUILD
+1. We are going to split our application into two implimentations. 
+2. Version 1- Firstly, we are going to back a simple and straight forward app. 
+3. We want to show a text input at the top which a user can use. After this, we are going to do a research or search to an external API. 
+4. Version 2- We are going to do the same stuff as in version 1. 
+5. The difference will be how we show this to the user. We want to tile images across the screen. We want to figure out how to make different size images end up fitting well within our application.
+*/
+
+
+
